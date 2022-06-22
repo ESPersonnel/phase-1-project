7e4c3257b00899596f3778b0bf718568c7ca3c96
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-06-22 16:16:42 +0300

    added displayWeather function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,40 @@
 let weather = {
-    'apiKey': '834b35552fcfb0c0071704a4c517816d',
-    'apiUrl': function(){
-        fetch(`https://api.openweathermap.org/data/3.0/`)
-    }.then((response) => response.json())
-    .then((data) => console.log(data))
+    apiKey: '834b35552fcfb0c0071704a4c517816d',
+    fetchWeather: function(city){
+        fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apiKey}`
+        )
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data))
+    },
+    displayWeather: function(data){
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name, icon, description, temp, humidity, speed);
+        document.querySelector('.city').innerText = "Weather in " + name;
+        document.querySelector('.icon').src = 'https://openweathermap.org/img/wn/'+ icon +'.png';
+        document.querySelector('.description').innerText = description;
+        document.querySelector('.temp').innerText = temp + "°C";
+        document.querySelector('.humidity').innerText = "Humidity: " + humidity + "%";
+        document.querySelector('.wind').innerText = "Wind Speed: " + speed + "km/h";
+        document.querySelector('.weather').classList.remove("loading");
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/random/1920x1080?" + name + "')";
+    },
+    search: function(){
+        this.fetchWeather(document.querySelector('.search-bar').value)
+    }
 }
+
+document.querySelector(".search button").addEventListener("click", function() {
+    weather.search();
+});
+
+document.querySelector(".search-bar").addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+        weather.search();
+    }
+})
+
+weather.fetchWeather("Nairobi")
