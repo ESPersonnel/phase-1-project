23d971da9b9d2688e41032af82c79978f39b40b8
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-06-23 20:47:21 +0300

    Fixing bugs in displayWeather()

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ let weather = {
         document.querySelector('.humidity').innerText = "Humidity: " + humidity + "%";
         document.querySelector('.wind').innerText = "Wind Speed: " + speed + "km/h";
         document.querySelector('.weather').classList.remove("loading");
-        document.body.style.backgroundImage = "url('https://source.unsplash.com/random/1920x1080?" + name + "')";
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/random/1920x1080?" + name + ",sky,landscape')";
     },
     search: function(){
         this.fetchWeather(document.querySelector('.search-bar').value)
