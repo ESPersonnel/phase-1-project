75f2c8b9e1aed54ce18abce1b08e56341e1cd1bd
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-07-06 22:47:13 +0300

    testing out date/time conversion from epoch

---
+++ test.js
@@ -0,0 +1,5 @@
+new Date().toLocaleDateString('en-UK', {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'});
+console.log(new Date().toLocaleDateString('en-UK', {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'}));
+console.log(new Date().toLocaleDateString('en-UK', {weekday: 'long'}));
+
+console.log(new Date().toLocaleTimeString('en-UK', {hour: 'numeric', minute: 'numeric', second: 'numeric'}));
