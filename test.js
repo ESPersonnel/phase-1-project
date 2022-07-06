3161908a105cdc59836c2d862584c978e0015b15
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-07-06 23:33:32 +0300

    Added comments

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
 new Date().toLocaleDateString('en-UK', {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'});
 console.log(new Date().toLocaleDateString('en-UK', {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'}));
+// Wednesday, 6 July 2022
 console.log(new Date().toLocaleDateString('en-UK', {weekday: 'long'}));
-
+// Wednesday
 console.log(new Date().toLocaleTimeString('en-UK', {hour: 'numeric', minute: 'numeric', second: 'numeric'}));
+// 23:13:02
